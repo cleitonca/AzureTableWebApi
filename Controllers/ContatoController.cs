afa9fd7ffd8188547cdce9000a65981394166079
Cleiton Araújo <cltn.info@gmail.com>
2023-02-20 18:54:30 -0300

    Projeto inicial

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AzureTableWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+       
+    }
+}
