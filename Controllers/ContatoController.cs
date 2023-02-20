c61cab0e11e2f4d3e7ed4e5579b75ecc54b28f4e
Cleiton Araújo <cltn.info@gmail.com>
2023-02-20 18:59:55 -0300

    Configurações Strings

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -6,6 +6,16 @@ namespace AzureTableWebApi.Controllers
     [Route("[controller]")]
     public class ContatoController : ControllerBase
     {
-       
+        private readonly string _connectionString;
+        private readonly string _tableName;
+
+        public ContatoController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("SAConnectionString");
+            _tableName = configuration.GetValue<string>("AzureTableName");
+
+
+        }
+
     }
 }
