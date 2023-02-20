c4fbb0a3cf183d509185f1c7de03a9e410dd1e08
Cleiton Araújo <cltn.info@gmail.com>
2023-02-20 19:03:37 -0300

    Classe Contato

---
+++ Models/Contato.cs
@@ -0,0 +1,13 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace AzureTableWebApi.Models
+{
+    public class Contato : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
